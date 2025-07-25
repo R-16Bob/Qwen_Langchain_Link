bb3e89a21f39cd79670fe703909db5b72cfe0d1e
R-16Bob <15343443062@163.com>
2025-07-25 14:27:53 +0800

    update: pdf_qa完成

--- app/pdf_qa.py
+++ app/pdf_qa.py
@@ -4,32 +4,31 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.qa_utils import qa_agnet
-from langchain.memory import ConversationBufferMemory
 
 st.title("📑AI智能PDF问答工具")
 
-if "memory" not in st.session_state:
-    st.session_state["memory"] = ConversationBufferMemory(
-        return_messages=True,
-        memory_key="chat_history",
-        output_key="answer"
-    )
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = []
 
 uploaded_file = st.file_uploader("请上传PDF文件", type=["pdf"])
+k = st.slider("检索文档数量：", value=2, min_value=1, max_value=4, step=1)  
+chunk_size = st.slider("文档分块大小：", value=500, min_value=200, max_value=1000, step=100)
 question = st.text_input("对PDF的内容进行提问", disabled=not uploaded_file)
 
 if uploaded_file and question:
     with st.spinner("AI正在思考中..."):
-        response = qa_agnet(uploaded_file, question, st.session_state["memory"])
+        response = qa_agnet(uploaded_file, question, chunk_size, k)
     st.write("### 答案")
-    st.write(response["answer"])
-    st.session_state["chat_history"] = response["chat_history"]
+    st.write(response)
+    st.session_state["chat_history"].append({"question": question, "answer": response})
 
 if "chat_history" in st.session_state:
     with st.expander("历史消息"):
-        for i in range(0, len(st.session_state["chat_history"]), 2):
-            st.write(st.session_state['chat_history'][i])
-            st.write(st.session_state['chat_history'][i+1])
-            if i<len(st.session_state["chat_history"])-2:
+        for i in range(0, len(st.session_state["chat_history"])):
+            q = st.session_state['chat_history'][i]["question"]
+            a = st.session_state['chat_history'][i]["answer"]
+            st.write(q)
+            st.write(a)
+            if i<len(st.session_state["chat_history"])-1:
                 st.divider()
         
--- utils/qa_utils.py
+++ utils/qa_utils.py
@@ -5,40 +5,51 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_community.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
-from utils.model_utils import load_embedding_model, load_chat_model
+from utils.model_utils import load_embedding_model, load_chat_model, apply_chat_template
+from langsmith import traceable
 
-def qa_agnet(uploaded_file, question, memory):
+"""
+这是一个RAG应用，我认为，并不需要多轮对话的记忆。因为它实际上是一个搜索引擎，就算有记忆，还是会不断检索，而且token长度增长很快
+。
+"""
+
+def generate_rag_message(context, question, tokenizer, enable_thinking=False):
+    system_message = {
+        "role": "system", "content": f"""使用以下上下文片段来回答用户的问题。
+如果你不知道答案，就直接说不知道，不要试图编造答案。\n----------------\n{context}"""}
+    message = [system_message, {"role": "user", "content": question}]
+    return apply_chat_template(message, tokenizer, enable_thinking)
+
+@traceable
+def qa_agnet(uploaded_file, question, chunk_size=500, k=2):  # k表示检索的文档数量
     # 加载模型
     model, tokenizer = load_chat_model(r"D:\AI_models\Qwen2.5-0.5B-Instruct")
     embeddings_model = load_embedding_model("Qwen3-Embedding-0.6B")
-    # 先保存上传的文件，再加载
+    # 保存上传的文档
     os.makedirs("cache", exist_ok=True)
-
     file_content = uploaded_file.read()
     file_path = os.path.join("cache", "temp.pdf")
     with open(file_path, "wb") as f:
         f.write(file_content)
-    # 加载文档
+
+    # 1. 加载文档
     loader = PyPDFLoader(file_path)
     docs = loader.load()
-    # split
+    # 2. split
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=500,
+        chunk_size=chunk_size,
         chunk_overlap=50,
         separators=["\n\n", "\n", "。", "！", "？" , "，", "、", ""]
     )
     split_docs = text_splitter.split_documents(docs)
-    # embedding
-
-    # vectorstore
+    # 3. vectorstore
     db = FAISS.from_documents(split_docs, embeddings_model)
     retriever = db.as_retriever()
-
-    # chain
-    qa = ConversationalRetrievalChain.from_llm(
-        llm=model,
-        retriever=retriever,
-        memory=memory
-    )
-    response = qa.invoke({"chat_history": memory, "question": question})
-    return response
+    # 4. 执行检索
+    retrieved_docs = (retriever.invoke(question))[:k] # 选择前k条结果
+    print(f"检索结果:{retrieved_docs}")
+    # 5. 生成消息
+    context = "\n".join([doc.page_content for doc in retrieved_docs])
+    message = generate_rag_message(context, question, tokenizer)
+    response = model.invoke(message)
+    return response.content
